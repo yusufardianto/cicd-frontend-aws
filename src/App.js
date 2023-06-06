de54f0cbc5986babe928641beeb5ee73e3d1e13b
Muhammad Yusuf Ardianto <56336225+yusufardianto@users.noreply.github.com>
2023-06-06 15:52:48 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./components/UserList";
 import AddUser from "./components/AddUser";
 import EditUser from "./components/EditUser";
+import Home from "./components/Hello";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<UserList/>}/>
-        <Route path="add" element={<AddUser/>}/>
-        <Route path="edit/:id" element={<EditUser/>}/>
+        <Route path="/" element={<UserList />} />
+        <Route path="/hello" element={<Home />} />
+        <Route path="add" element={<AddUser />} />
+        <Route path="edit/:id" element={<EditUser />} />
       </Routes>
     </BrowserRouter>
   );
