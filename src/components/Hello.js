aed5d563056dc54a034675fe8a34bfe5f0f65c43
Muhammad Yusuf Ardianto <56336225+yusufardianto@users.noreply.github.com>
2023-06-06 17:04:44 +0700

    hello selasa

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 // import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h1>hello world</h1>;
+  const [hello, getHello] = useState([]);
+
+  useEffect(() => {
+    getHello();
+  }, []);
+
+  return <h1>hello selasa</h1>;
+  //   const getHello = async () => {
+  //     const response = await axios.get("http://localhost:5000/helloo");
+  //     setUser(response.data);
+  //   };
 };
 export default Home;
